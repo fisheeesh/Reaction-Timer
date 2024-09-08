2774abda6b5a3a5779ef9c5d17c2d2b4d9a5b631
fisheeesh <swanphyo444@gmail.com>
2024-09-08 14:34:32 +0700

    game setUp

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-
+  <h1>How fast can you catch me?</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <div v-if="isPlaying">
+    <Block :delay = "delay"/>
+  </div>
 </template>
 
 <script>
+import Block from './components/Block.vue';
 export default {
-
+  data(){
+    return{
+      isPlaying: false,
+      delay : null,
+    }
+  },
+  methods : {
+    start(){
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+      console.log(this.isPlaying)
+    }
+  },
+  components: {
+    Block
+  }
 }
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="block">
+    Click Here {{ delay }}
+  </div>
+</template>
+
+<script>
+export default {
+  props : [
+    'delay'
+  ],
+}
+</script>
+
+<style>
+.block{
+  width: 400px;
+  height: 300px;
+  padding-top: 20px;
+  background-color: cyan;
+  border-radius: 20px;
+  margin: 30px auto;
+}
+</style>
